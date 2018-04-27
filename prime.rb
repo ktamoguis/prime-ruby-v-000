9b68a14e6dc3afd7853a63086bc14469876ed397
Karl Amoguis <ktamoguis@gmail.com>
2018-04-27 06:55:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
 # Add  code here!
-def prime()
-
+def prime(number)
+  i = 2
+  if number < 2  then
+    false
+  else
+    while i < divnumber 
+      if number % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
 end
